2bbb2080b4b2ab2774f78defa3cd67ee5a82ba4b
RodrigoPadilla <97059026+RodrigoPadilla@users.noreply.github.com>
2023-05-28 17:08:46 -0300

    Creacion DB y gitignore

---
+++ SpokeToTheManager/SpokeToTheManager/Models/UserContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SpokeToTheManager.Models
+{
+    public class UserContext : DbContext
+    {
+        public DbSet<UsuarioModelo> User { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder
+       optionsBuilder)
+        {
+
+            optionsBuilder.UseSqlServer(@"Server=.\SQLEXPRESS;Database=SpokeToTheManager
+;Trusted_Connection=True;TrustServerCertificate=true");
+        }
+    }
+}
+
---
+++ SpokeToTheManager/SpokeToTheManager/Models/UsuarioModelo.cs
@@ -0,0 +1,12 @@
+﻿namespace SpokeToTheManager.Models
+{
+    public class UsuarioModelo
+    {
+        public int id { get; set; }
+        public String nombreUsuario { get; set; }
+        public String contrasenia { get; set; }
+        public String mail { get; set; }
+
+
+    }
+}
