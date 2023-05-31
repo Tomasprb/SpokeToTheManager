84d246407e998b0eacfdec2bd67bcbd0bd2d87bc
RodrigoPadilla <97059026+RodrigoPadilla@users.noreply.github.com>
2023-05-31 20:30:20 -0300

    Creacion

---
+++ SpokeToTheManager/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace SpokeToTheManager.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public String Nombre { get; set; }
+        public String Email { get; set; }
+        public String Contrasenia { get; set; }
+    }
+}
---
+++ SpokeToTheManager/Models/UserContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SpokeToTheManager.Models
+{
+    public class UserContext : DbContext
+    {
+        public DbSet<User> Usuarios { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder
+       optionsBuilder)
+        {
+
+            optionsBuilder.UseSqlServer(@"Server=.\SQLEXPRESS;Database=SpokeToTheManagerDBCF
+;Trusted_Connection=True;TrustServerCertificate=true");
+        }
+    }
+}
