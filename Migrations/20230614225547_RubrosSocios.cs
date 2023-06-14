45e64a6d2a2469ef6225285c09110ac1b9ec720f
Tomas Prott <44026752+Tomasprb@users.noreply.github.com>
2023-06-14 19:57:41 -0300

    agregados rubros y socios

---
+++ Migrations/20230614225547_RubrosSocios.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SpokeToTheManager.Migrations
+{
+    /// <inheritdoc />
+    public partial class RubrosSocios : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "rubros",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_rubros", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "socios",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Telefono = table.Column<double>(type: "float", nullable: false),
+                    RubroId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_socios", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_socios_rubros_RubroId",
+                        column: x => x.RubroId,
+                        principalTable: "rubros",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_socios_RubroId",
+                table: "socios",
+                column: "RubroId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "socios");
+
+            migrationBuilder.DropTable(
+                name: "rubros");
+        }
+    }
+}
