b84c38308ebb437c51db4f62da38ec229caf5a67
Tomas Prott <44026752+Tomasprb@users.noreply.github.com>
2023-07-08 20:15:13 -0300

    iniciado el login

--- Program.cs
+++ Program.cs
@@ -1,11 +1,16 @@
 using SpokeToTheManager.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authentication.Cookies;
 
 
 var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<UserContext>(options => options.UseSqlServer(@"Server=.\SQLEXPRESS;Database=SpokeToTheManagerDBCF;Trusted_Connection=True;TrustServerCertificate=true"));
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie(option=>{
+    option.LoginPath="/acceso/login";
+    option.ExpireTimeSpan = TimeSpan.FromMinutes(20);
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -17,13 +22,12 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
+
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Resumenes}/{action=Index}");
-app.MapControllerRoute(
-    name: "login",
-    pattern: "{controller=Home}/{action=Login}");
+    pattern: "{controller=acceso}/{action=login}/{id?}");
 
 app.Run();
